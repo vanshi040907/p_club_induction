4f51aeec88df8be453994a048aa6253ea752e5f6
vanshi040907 <vanshi040907@gmail.com>
2026-03-17 17:51:58 +0530

    components added

--- induction_task/src/App.jsx
+++ induction_task/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Lost_card from './components/lost_card.jsx'
 
 function App() {
   
-
   return (
     <>
-    
+    <Lost_card/>
     </>
   )
 }
